0dc7efbeb2ce44eedd6027f5edc5128d77ac4e99
Arpita Das <dasarpita0369@gmail.com>
2026-06-21 16:57:23 +0530

    Update LoginEasyCalculationTest.java

--- LoginEasyCalculationTest.java
+++ LoginEasyCalculationTest.java
@@ -1,5 +1,31 @@
 package com.Framework.TestCase;
 
-public class LoginEasyCalculationTest {
+import java.io.IOException;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.Framework.pages.BaseClass;
+import com.Framework.pages.LoginPage;
+
+import utility.ExcelDataProvider;
+import utility.Helper;
+
+public class LoginEasyCalculationTest extends BaseClass {
+	
+	
+	@Test
+	public void LoginTest() throws Exception {
+		
+		ExcelDataProvider excel = new ExcelDataProvider();
+		excel.getStringData("sheet1", 0, 0);
+		LoginPage login = PageFactory.initElements(driver, LoginPage.class);
+		
+		login.LoginEasyCalculation(excel.getStringData("sheet1", 0, 0),excel.getStringData("sheet1", 0, 1));
+		Thread.sleep(10000);
+	
+	}
+	
 
 }
+
